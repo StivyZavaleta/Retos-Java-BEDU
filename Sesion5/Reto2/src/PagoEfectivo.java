c41859902f646d67d274c003b7528c39a4a8b997
Stivy Zavaleta <71535080+StivyZavaleta@users.noreply.github.com>
2025-05-14 23:39:39 -0700

    Sesion 5
    Actualizacion de Reto 2

---
+++ Sesion5/Reto2/src/PagoEfectivo.java
@@ -0,0 +1,16 @@
+public class PagoEfectivo extends MetodoPago {
+
+    public PagoEfectivo(double monto) {
+        super(monto);
+    }
+
+    @Override
+    public void procesarPago() {
+        System.out.println("💵 Procesando pago en efectivo por $" + monto);
+    }
+
+    @Override
+    public boolean autenticar() {
+        return true;
+    }
+}
